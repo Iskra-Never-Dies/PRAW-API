c08d05af5e951b79a21c9f742bd8f1170fb36f99
Iskra-Never-Dies <some1onyt@gmail.com>
2021-09-24 23:54:23 +0200

    Add project files.

---
+++ PRAW/src/GateWay/DiscordEvents/DiscordEvent.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace PRAW
+{
+    internal class DiscordEvent
+    {
+        [JsonProperty("op")]
+        public GatewayOpcodes OpCode { get; set; }
+
+        // if data is string then newtonsoft shits its self cuz its made by a bunch of fucking retards
+        [JsonProperty("d")]
+        public object? Data { get; set; }
+
+        [JsonProperty("s")]
+        public uint? Sequence { get; set; }
+
+        [JsonProperty("t")]
+        public string? EventType { get; set; }
+    }
+}
---
+++ PRAW/src/_Extensions/Extension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRAW
+{
+    internal static class Extension
+    {
+        /// <summary>
+        /// MDAFR: ms devs are fucking retards
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns>ToString of <param name="val"></returns>
+        //public static string? MDAFR(this object val) =>
+        //    val?.ToString();
+
+        internal static T Modify<T>(this T Generic, Action<T> _Action)
+        {
+            _Action(Generic);
+            return Generic;
+        }
+    }
+}
